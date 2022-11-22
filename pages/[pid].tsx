2c94f1bfdc778481c5bca18290cbf993d010982c
andyvy <thienvy95@gmail.com>
2022-11-22 22:05:30 +0700

    update

--- pages/[pid].tsx
+++ pages/[pid].tsx
@@ -13,6 +13,10 @@ const ProductDetailPage: NextPage<IProductDetailPageProps> = (props) => {
 
     const { loadedProduct } = props;
 
+    if (!loadedProduct) {
+        return <p>Loading...</p>
+    }
+
     return (
         <Fragment>
             <h1>{loadedProduct.title}</h1>
@@ -21,20 +25,45 @@ const ProductDetailPage: NextPage<IProductDetailPageProps> = (props) => {
     )
 }
 
-export const getStaticProps = async (context: GetStaticPropsContext<ParsedUrlQuery, { product: IProduct }>) => {
-
-    const { params } = context;
-    const productId = params?.id
-
+async function getData () {
     const filePath = path.join(process.cwd(), 'data', 'dummy-backend.json');
     const jsonData = await fs.readFile(filePath, 'utf-8');
-
     const data = JSON.parse(jsonData);
+    return data;
+}
+
+export const getStaticPaths = async () => {
+    const data = await getData();
+    const ids = data.products.map((p: IProduct) => p.id);
+    const pathsWithParams = ids.map((id: string) => ({
+        params: {
+            pid: id
+        }
+    }))
+    return {
+        paths: pathsWithParams,
+        fallback: true
+    }
+}
+
+export const getStaticProps = async (context: GetStaticPropsContext<ParsedUrlQuery, { loadedProduct: IProduct }>) => {
+
+    const { params } = context;
+    const productId = params?.pid
+
+    const data = await getData();
     const product = data.products.find((product: IProduct) => product.id === productId);
+
+    if (!product) {
+        return {
+            notFound: true
+        }
+    }
 
     return {
         props: {
-            product
+            loadedProduct: product,
+            
         }
     }
 }
