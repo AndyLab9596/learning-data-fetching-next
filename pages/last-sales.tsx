827818b72d3cadd153a84f8ca17be7733d4056c4
andyvy <thienvy95@gmail.com>
2022-11-24 07:22:18 +0700

    Update

--- pages/last-sales.tsx
+++ pages/last-sales.tsx
@@ -1,32 +1,53 @@
+import { GetStaticPropsContext, NextPage } from 'next';
 import React, { useEffect, useState } from 'react'
+import useSWR from 'swr';
 
-const LastSalesPage = () => {
-    const [sales, setSales] = useState<{[x: string]: string | number}[]>([]);
-    const [isLoading, setIsLoading] = useState(false);
+const fetcher = (url: string) => fetch(url).then(res => res.json())
+
+interface ILastSalesPage {
+    sales:{[x: string]: string | number}[]
+}
+
+const LastSalesPage : NextPage<ILastSalesPage> = (props) => {
+    const [sales, setSales] = useState<{ [x: string]: string | number }[]>(props.sales);
+    // const [isLoading, setIsLoading] = useState(false);
+
+    const { data, error } = useSWR('https://next-practice-1-default-rtdb.asia-southeast1.firebasedatabase.app/sales.json', fetcher)
 
     useEffect(() => {
-        setIsLoading(true)
-        fetch('https://next-practice-1-default-rtdb.asia-southeast1.firebasedatabase.app/sales.json')
-            .then((res) => res.json())
-            .then((data) => {
+        if (data) {
+            const transformedSales = [];
 
-                const transformedSales = [];
+            for (const key in data) {
+                transformedSales.push({ id: key, username: data[key].username, volume: data[key].volume })
+            }
+            setSales(transformedSales)
+        }
+    }, [data])
 
-                for (const key in data) {
-                    transformedSales.push({ id: key, username: data[key].username, volume: data[key].volume })
-                }
+    // useEffect(() => {
+    //     setIsLoading(true)
+    //     fetch('https://next-practice-1-default-rtdb.asia-southeast1.firebasedatabase.app/sales.json')
+    //         .then((res) => res.json())
+    //         .then((data) => {
 
-                setSales(transformedSales);
-                setIsLoading(false);
-            })
-    }, [])
+    //             const transformedSales = [];
 
-    if (isLoading) {
+    //             for (const key in data) {
+    //                 transformedSales.push({ id: key, username: data[key].username, volume: data[key].volume })
+    //             }
+
+    //             setSales(transformedSales);
+    //             setIsLoading(false);
+    //         })
+    // }, [])
+
+    if (error) {
+        return <p>fail to load</p>
+    }
+
+    if (!data && !sales) {
         return <p>Loading ...</p>
-    } 
-
-    if (!sales) {
-        return <p>No data yet</p>
     }
 
     return (
@@ -38,6 +59,25 @@ const LastSalesPage = () => {
             ))}
         </ul>
     )
+}
+
+export const getStaticProps = async (context: GetStaticPropsContext) => {
+    const response = await fetch('https://next-practice-1-default-rtdb.asia-southeast1.firebasedatabase.app/sales.json');
+    const data = await response.json();
+    const transformedSales = [];
+
+
+    for (const key in data) {
+        transformedSales.push({ id: key, username: data[key].username, volume: data[key].volume })
+    }
+    return {
+        props: {
+            sales: transformedSales
+        },
+        revalidate: 10
+    }
+
+
 }
 
 export default LastSalesPage
